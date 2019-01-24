d5ded23a6a2ddf0f199356fc0ce7be665cf53af1
Skobelev Aleksei <webpro3@masproject.pro>
2019-01-24 13:31:32 +0800

    разбор стрелочной функции

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org/',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+    }
+];
 
 class App extends Component {
   render() {
       const helloWord = 'Добро пожаловать в Путь к изучению React';
-      let user = {
-          name: 'Иван',
-          lastName: 'Иванов',
-      };
       return (
       <div className="App">
         <h2>{helloWord}</h2>
-        <div className="user_name">{user.lastName +' '+ user.name}</div>
+          {list.map(item =>
+                    <div key={item.objectID}>
+                          <span>
+                              <a href={item.url}>{item.title}</a>
+                          </span>
+                        <span>{item.author}</span>
+                        <span>{item.num_comments}</span>
+                        <span>{item.points}</span>
+                    </div>
+
+
+          )}
       </div>
     );
   }
